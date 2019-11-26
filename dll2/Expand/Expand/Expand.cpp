b1d0bb1a889c9fc3fbac33c137d1a248c177dc05
zhuying-cc <55731381+zhuying-cc@users.noreply.github.com>
2019-11-26 14:49:48 +0800

    动态库实现与显式调用

---
+++ dll2/Expand/Expand/Expand.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+typedef void (*PINIT)(const unsigned long long a, const unsigned long long b);
+typedef bool (*PNEXT)();
+typedef unsigned long long (*PCURRENT)();
+typedef unsigned (*PINDEX)();
+
+int main()
+{
+	// Initialize a Fibonacci relation sequence.
+
+	HMODULE hDLL = LoadLibrary(L"MathLibrary.dll");
+
+	if (hDLL == NULL) {
+		cout << "获取动态链接库失败！";
+		return 0;
+	}
+
+	PINIT fibonacci_init = (PINIT)GetProcAddress(hDLL, "fibonacci_init");
+	PNEXT fibonacci_next = (PNEXT)GetProcAddress(hDLL, "fibonacci_next");
+	PCURRENT fibonacci_current = (PCURRENT)GetProcAddress(hDLL, "fibonacci_current");
+	PINDEX fibonacci_index = (PINDEX)GetProcAddress(hDLL, "fibonacci_index");
+
+
+	fibonacci_init(1, 1);
+	// Write out the sequence values until overflow.
+	do {
+		std::cout << fibonacci_index() << ": "
+			<< fibonacci_current() << std::endl;
+	} while (fibonacci_next());
+	// Report count of values written before overflow.
+	std::cout << fibonacci_index() + 1 <<
+		" Fibonacci sequence values fit in an " <<
+		"unsigned 64-bit integer." << std::endl;
+
+	FreeLibrary(hDLL);
+
+	return 0;
+}
+
